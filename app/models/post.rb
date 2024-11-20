4a7db04753154887395ca5318b4b60c555bb9903
brazhnyknorman <brazhnyknorman@gmail.com>
2024-11-20 16:02:27 -0500

    Add One-to-Many association between User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 1..50 }
   validates :link, presence: true, length: { minimum: 2 }
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   }
 
   validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 end
